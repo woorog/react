98b15a19d5f11f85956876235990c88c6af447c7
Hyochang098 <wlsgyckd7@naver.com>
2025-07-22 22:44:56 +0900

    feat: useParams

--- hyo/frontend/project/src/pages/todo/listPage.tsx
+++ hyo/frontend/project/src/pages/todo/listPage.tsx
@@ -1,7 +1,22 @@
+import { useSearchParams } from "react-router";
+
 function ListPage() {
+    
+        const [queryParams] = useSearchParams();
+        
+        const page: string | null = queryParams.get("page");
+        const size: string | null = queryParams.get("size");
+        
+        console.log("Page:", page);
+        console.log("Size:", size);
+        console.log("Query Params:", queryParams.toString());
+
     return(
+
+
+
         <div className="bg-white w-full">
-            <div className="text-4xl">Todo List Page</div>
+            <div className="text-4xl">Todo List Page {page} {size} </div>
         </div>
     );
 }
